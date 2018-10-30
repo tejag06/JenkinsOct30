61a65cf65bebf35c9847191a4a5aac8671f35530
Gangula <teja.gangula@accenture.com>
2018-10-30 15:47:31 +0530

    Java files commit message

---
+++ src/main/java/FirstJenkinsJob/SampleAppJenkins/App.java
@@ -0,0 +1,14 @@
+package FirstJenkinsJob.SampleAppJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World 2!" );
+    }
+}
